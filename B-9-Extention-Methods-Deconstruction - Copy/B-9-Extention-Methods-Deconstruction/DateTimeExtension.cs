e89f5b4470a70cbc3a48c9e737ea1f0c55738f43
MaryLapko <mary.putilina@gmail.com>
2019-01-28 02:39:23 +0300

    first commit

---
+++ B-9-Extention-Methods-Deconstruction - Copy/B-9-Extention-Methods-Deconstruction/DateTimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExtensions
+{
+    public static class DateTimeExtension
+    {
+        public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hour, out int minute, out int second)
+        {
+            year = dateTime.Year;
+            month = dateTime.Month;
+            day = dateTime.Day;
+            hour = dateTime.Hour;
+            minute = dateTime.Minute;
+            second = dateTime.Second;
+        }
+    }
+}
---
+++ B-9-Extention-Methods-Deconstruction - Copy/B-9-Extention-Methods-Deconstruction/Practice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DateTimeExtensions;
+
+namespace Base_Lesson_9.Practice
+{
+    public static class Practice
+    {
+        /// <summary>
+        /// L9-P-EX-1/2
+        /// Создать структуру Coordinates (трехмерных). 
+        /// Добавить экземплярный метод деконструкции значений координат.
+        /// Вывести на консоль длину вектора по координатам.
+        /// </summary>
+        public static void L9_P_EX_1_from_2()
+        {
+
+        }
+
+        /// <summary>
+        /// L9-P-EX-2/2. 
+        /// Создать деконструктор для обьекта DateTime 
+        /// (год, месяц, день, час, минуты, секунды). 
+        /// Получить и вывести на консоль текущее время.
+        /// Использовать пустые параметры.
+        /// </summary>
+        /// 
+
+        public static void L9_P_EX_2_from_2()
+        {
+            var dateTime = DateTime.Now;
+            var (year, month, day, _, _, _ ) = dateTime;         
+            Console.WriteLine(day + "." + month + "." + year);
+         
+
+        }
+    }
+}
